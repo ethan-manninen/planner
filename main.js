45bb101738dfc525afa8b4f700e3bfa6c6c82428
Ethan <ethanamanninen@gmail.com>
2025-01-29 15:55:52 -0600

    Seperated js and css files, added server.py

---
+++ main.js
@@ -0,0 +1,24 @@
+const hour = new Date().getHours();
+const x = 13;
+let greeting;
+    if (0 < hour && hour < 12) {
+        greeting = "Good Morning";
+    } else if (12 <= hour && hour <= 17) {
+        greeting = "Good Afternooon!";
+    } else {
+        greeting = "Good Evening!";
+    }
+    document.getElementById("greeting").innerHTML = greeting;
+/*
+    const alert_div = document.createElement("div");
+    alert_div.setAttribute("class", "alert");
+
+    const alert_container = document.getElementById("alerts");
+    alert_container.appendChild(alert_div);
+*/
+
+fetch('http://localhost:1337')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); // { message: 'Hello from Python!' }
+  });
