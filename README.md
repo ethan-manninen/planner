b9e8b02de5fe11cfe62118ed08f697098f79d9fc
Ethan <ethanamanninen@gmail.com>
2025-01-30 07:43:41 -0600

    restored files

---
+++ README.md
@@ -0,0 +1,2 @@
+# The best dashboard page you will ever see
+work in progress
---
+++ server.py
@@ -0,0 +1,19 @@
+import http.server
+import socketserver
+import json
+
+PORT = 1337
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        data = {'message': 'Hello from Python!'}
+        self.wfile.write(json.dumps(data).encode())
+
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
+# Im not sure if I like this implamentation, copyed from google ai websearch thing...
