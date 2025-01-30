ce9f4799c38916605249cc11976248d970a5737a
Ethan <ethanamanninen@gmail.com>
2025-01-30 13:46:15 -0600

    Added basic styling of news page

--- server.py
+++ server.py
@@ -4,16 +4,17 @@ import json
 import config
 from newsapi import NewsApiClient
 
-PORT = 1337 
+PORT = 1337
 
-data = {'message': 'Hello from Python!'}
+newsapi = NewsApiClient(api_key=config.API_KEY)
+
+data = newsapi.get_top_headlines(category='technology',language='en')
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-type', 'application/json','Access-Control-Allow-Origin: *')
         self.end_headers()
-        
         self.wfile.write(json.dumps(data).encode())
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
